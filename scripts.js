f754425e203f89d9d50fe7067cc8e61de04e2432
avkap007 <avni_kapoor@sfu.ca>
2023-10-21 11:37:50 -0700

    Added basic bootstrap skeleton

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Function to transition between pages
+function startProject() {
+    $('#page1').fadeOut(300, function() {
+        $('#page2').fadeIn(300);
+        // Optional: Add any actions to initiate when page 2 is displayed
+    });
+}
+
+// Actions to perform when the document is ready
+$(document).ready(function() {
+    // Optional: Fetch a motivational quote or any other action
+});
+
+// Additional JavaScript functions for interactive elements can be added here
