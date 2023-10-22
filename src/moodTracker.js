93007eaf18ab809ff4173b499e6c82286666c470
mantajdhillon <mkd21@sfu.ca>
2023-10-21 17:59:26 -0700

    mood tracker debugging removed

--- src/moodTracker.js
+++ src/moodTracker.js
@@ -12,28 +12,22 @@ function getReccomendation() {
 
 
     if(moodHappy.checked){
-        alert("I'M HAPPY")
         changeText.textContent = "you should: give someone a compliment";
     }
     if(moodCalm.checked){
-        alert("I'M Calm")
         changeText.textContent = "you should: go catch up with a loved one";
 
     }
     if(moodUpset.checked){
-        alert("I'M upset")
         changeText.textContent = "you should: talk to someone you trust";
 
     }
     if(moodAngry.checked){
-        alert("I'M ANGRY")
         changeText.textContent = "you should: go for a quick walk and do some thinking";
 
     }
     if(moodAnxious.checked){
-        alert("I'M AnXiOuS")
         changeText.textContent = "you should: take some deep breaths";
-
     }
 
 };
