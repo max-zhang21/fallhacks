42101e27f32aab7e27c0c59530c8244777e6a0f2
mantajdhillon <mkd21@sfu.ca>
2023-10-21 17:30:27 -0700

    Add a js file to provide recs

---
+++ src/moodTracker.js
@@ -0,0 +1,15 @@
+    // Function to provide a reccomendation based on mood
+    function getReccomendation() {
+        document.getElementByID("reccomendation").innerHTML = "HELLO WORLD";
+        var ele = document.getElementsByTagName('input');
+        // for (i = 0; i < ele.length; i++) {
+        //     if (ele[i].type = "radio") {
+
+        //         if (ele[i].checked)
+        //             document.getElementById("reccomendation").innerHTML
+        //                 += ele[i].name + " Value: "
+        //                 + ele[i].value + "<br>";
+        //     }
+        // }
+    }
+
