98f12e20933285befe36202cab1d34ddfac80788
mantajdhillon <mkd21@sfu.ca>
2023-10-21 17:58:25 -0700

    Reccomendations are given based on mood

--- src/moodTracker.js
+++ src/moodTracker.js
@@ -1,15 +1,39 @@
-    // Function to provide a reccomendation based on mood
-    function getReccomendation() {
-        document.getElementByID("reccomendation").innerHTML = "HELLO WORLD";
-        var ele = document.getElementsByTagName('input');
-        // for (i = 0; i < ele.length; i++) {
-        //     if (ele[i].type = "radio") {
 
-        //         if (ele[i].checked)
-        //             document.getElementById("reccomendation").innerHTML
-        //                 += ele[i].name + " Value: "
-        //                 + ele[i].value + "<br>";
-        //     }
-        // }
+
+
+
+function getReccomendation() {
+    const moodHappy = document.getElementById("moodHappy");
+    const moodCalm = document.getElementById("moodCalm");
+    const moodUpset = document.getElementById("moodUpset");
+    const moodAngry = document.getElementById("moodAngry");
+    const moodAnxious = document.getElementById("moodAnxious");
+    const changeText = document.querySelector("#reccomendation");
+
+
+    if(moodHappy.checked){
+        alert("I'M HAPPY")
+        changeText.textContent = "you should: give someone a compliment";
+    }
+    if(moodCalm.checked){
+        alert("I'M Calm")
+        changeText.textContent = "you should: go catch up with a loved one";
+
+    }
+    if(moodUpset.checked){
+        alert("I'M upset")
+        changeText.textContent = "you should: talk to someone you trust";
+
+    }
+    if(moodAngry.checked){
+        alert("I'M ANGRY")
+        changeText.textContent = "you should: go for a quick walk and do some thinking";
+
+    }
+    if(moodAnxious.checked){
+        alert("I'M AnXiOuS")
+        changeText.textContent = "you should: take some deep breaths";
+
     }
 
+};
